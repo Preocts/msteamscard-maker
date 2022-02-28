c6167bdbb5916930ebc8faec794111e2f21de4df
Preocts <preocts@gmail.com>
2022-02-27 22:45:49 -0500

    Remove init imports

--- src/cardmaker/__init__.py
+++
@@ -1,9 +0,0 @@
-from cardmaker.model import elements
-from cardmaker.model import entities
-from cardmaker.webhookcard import WebhookCard
-
-__all__ = [
-    "WebhookCard",
-    "elements",
-    "entities",
-]
