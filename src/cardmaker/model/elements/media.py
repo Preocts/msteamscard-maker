08fa045208ac215535aefbf47aed6afb2d958ae0
Preocts <preocts@gmail.com>
2022-03-01 23:17:58 -0500

    Media element

---
+++ src/cardmaker/model/elements/media.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import dataclasses
+
+from cardmaker.model.base_element import BaseElement
+
+
+@dataclasses.dataclass(repr=False)
+class MediaSource:
+    mimeType: str
+    url: str
+
+
+@dataclasses.dataclass(repr=False)
+class Media(BaseElement):
+    """
+    Define a Media Card Element used inside `content.body[]`
+
+    Single media sources can be defined in the construction of object while
+    multiple media sources are added with `.add_source()`
+    """
+
+    type: str = "Media"
+    sources: list[MediaSource] = dataclasses.field(default_factory=list)
+    poster: str | None = None
+    altText: str | None = None
+
+    @classmethod
+    def basic_setup(
+        cls,
+        mime_type: str,
+        media_url: str,
+        poster_url: str | None = None,
+        altText: str | None = None,
+    ) -> Media:
+        """Build Media object with basic setup"""
+        newobj = cls()
+        newobj.add_source(mime_type, media_url)
+        newobj.poster = poster_url
+        newobj.altText = altText
+        return newobj
+
+    def add_source(self, mime_type: str, url: str) -> None:
+        """
+        Define a source for a Media element
+
+        Args:
+            mime_type: Mime type associated media (e.g. `video/mp4`)
+            url: URL to media
+        """
+        self.sources.append(MediaSource(mime_type, url))
+
+    def set_poster(self, url: str) -> None:
+        """URL of an image to display before playing"""
+        self.poster = url
+
+    def set_altText(self, text: str) -> None:
+        """Alternate text describing the audio or video"""
+        self.altText = text
---
+++ tests/model/elements/media_test.py
@@ -0,0 +1,68 @@
+from unittest.mock import patch
+
+from cardmaker.model.base_element import BaseElement
+from cardmaker.model.elements.media import Media
+
+EXPECTED_EMPTY = {
+    "type": "Media",
+    "sources": [],
+}
+
+EXPECTED_SOURCES = {
+    "mimeType": "video/mp4",
+    "url": "https://127.0.0.1",
+}
+
+
+def test_ensure_superclass() -> None:
+    assert issubclass(Media, BaseElement)
+
+
+def test_empty_model_asdict() -> None:
+    media = Media()
+
+    assert media.asdict() == EXPECTED_EMPTY
+
+
+def test_full_model_asdict() -> None:
+    media = Media()
+    with patch.dict(EXPECTED_EMPTY, {"sources": []}) as expected_full:
+        for _ in range(10):
+            expected_full["sources"].append(EXPECTED_SOURCES)
+            media.add_source(EXPECTED_SOURCES["mimeType"], EXPECTED_SOURCES["url"])
+
+        assert media.asdict() == expected_full
+
+
+def test_set_poster() -> None:
+    media = Media()
+    media.set_poster("https://127.0.0.1")
+
+    result = media.asdict()
+
+    assert result["poster"] == "https://127.0.0.1"
+
+
+def test_set_altText() -> None:
+    media = Media()
+    media.set_altText("Mock")
+
+    result = media.asdict()
+
+    assert result["altText"] == "Mock"
+
+
+def test_build_from_basic_setup() -> None:
+    with patch.dict(EXPECTED_EMPTY, {"sources": []}) as expected:
+        media = Media.basic_setup(
+            EXPECTED_SOURCES["mimeType"],
+            EXPECTED_SOURCES["url"],
+            "mock",
+            "mock",
+        )
+        expected["sources"].append(EXPECTED_SOURCES)
+        expected["poster"], expected["altText"] = ("mock", "mock")
+
+        result = media.asdict()
+
+        assert result == expected
