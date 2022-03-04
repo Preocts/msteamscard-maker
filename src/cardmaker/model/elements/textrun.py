d48fd603da19b5b9ce683d0c572897bd8fb6744a
Preocts <preocts@gmail.com>
2022-03-03 21:13:42 -0500

    TextRun object

---
+++ src/cardmaker/model/elements/textrun.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+
+import dataclasses
+
+from cardmaker.model.base_element import BaseElement
+from cardmaker.model.constants import COLORS
+from cardmaker.model.constants import FONTTYPES
+from cardmaker.model.constants import SIZE
+from cardmaker.model.constants import T_COLORS
+from cardmaker.model.constants import T_FONTTYPES
+from cardmaker.model.constants import T_SIZE
+from cardmaker.model.constants import T_WEIGHT
+from cardmaker.model.constants import WEIGHT
+
+
+@dataclasses.dataclass
+class TextRun(BaseElement):
+    """
+    Defines a TextRun element
+
+    Attributes can be set with keyword arguments, directly thought assigment
+    or use `set_[attrname]()` methods. All `None` assigned attributes are
+    excluded from final `.render()` of model.
+
+    Invalid values will default to `None` when set through keywords or setters.
+
+    Arguments:
+        text: [str] Text to display. Markdown is not supported
+
+    Keyword Arguments:
+        color: [str] Control the color of TextBlock elements
+        fontType: [str] Type of font to use for rendering
+        highlight: [bool] If true, displays the text highlighted
+        isSubtle: [bool] If true displays text slightly toned down
+        italic: [bool] If true, displays the text using italic font
+        size: [str] Control size of text
+        strikethrough: [bool] If true, displays the text with strikethrough
+        underline: [bool] If true, displays the text with an underline
+        weight: [str] Control the weight of TextBlock elements
+    """
+
+    type: str = "TextRun"
+    text: str = ""
+    color: str | None = None
+    fontType: str | None = None
+    highlight: bool | None = None
+    isSubtle: bool | None = None
+    italic: bool | None = None
+    size: str | None = None
+    strikethrough: bool | None = None
+    underline: bool | None = None
+    weight: str | None = None
+
+    def __init__(self, text: str, **kwargs: str | int | bool | None) -> None:
+        if not text:
+            raise ValueError("'text' is required when defining a TextRun")
+
+        self.text = text
+        for key, value in kwargs.items():
+            if not hasattr(self, f"set_{key}"):
+                raise KeyError(f"Invalid keyword arguement: '{key}'")
+            getattr(self, f"set_{key}")(value)
+
+    def set_color(self, color: T_COLORS | None) -> None:
+        """
+        Control the color of TextBlock elements
+            `default`, `dark`, `light`, `accent`, `good`, `warning`, `attention`
+        """
+        if color is not None and color not in COLORS:
+            color = None
+        self.color = color
+
+    def set_fontType(self, font_type: T_FONTTYPES | None) -> None:
+        """
+        Type of font to use for rendering
+            `default`, `monospace`
+        """
+        if font_type is not None and font_type not in FONTTYPES:
+            font_type = None
+        self.fontType = font_type
+
+    def set_highlight(self, flag: bool | None) -> None:
+        """If true, displays the text highlighted"""
+        self.highlight = bool(flag) if flag is not None else None
+
+    def set_isSubtle(self, flag: bool | None) -> None:
+        """If true, displays text slightly toned down to appear less prominent"""
+        self.isSubtle = bool(flag) if flag is not None else None
+
+    def set_italic(self, flag: bool | None) -> None:
+        """If true, displays the text using italic font"""
+        self.italic = bool(flag) if flag is not None else None
+
+    def set_size(self, size: T_SIZE | None) -> None:
+        """
+        Control size of text
+            `default`, `small`, `medium`, `large`, `extraLarge`
+        """
+        if size is not None and size not in SIZE:
+            size = None
+        self.size = size
+
+    def set_strikethrough(self, flag: bool | None) -> None:
+        """If true, displays the text with strikethrough"""
+        self.strikethrough = bool(flag) if flag is not None else None
+
+    def set_underline(self, flag: bool | None) -> None:
+        """If true, displays the text with underline"""
+        self.underline = bool(flag) if flag is not None else None
+
+    def set_weight(self, weight: T_WEIGHT | None) -> None:
+        """
+        Control the weight of TextBlock elements
+            `default`, `lighter`, `bolder`
+        """
+        if weight is not None and weight not in WEIGHT:
+            weight = None
+        self.weight = weight
---
+++ tests/model/elements/textrun_test.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+
+import pytest
+from cardmaker.model.elements.textrun import TextRun
+
+
+@pytest.fixture
+def textrun() -> TextRun:
+    return TextRun("mock")
+
+
+def test_init_setup(textrun: TextRun) -> None:
+    assert textrun.type == "TextRun"
+    assert textrun.text == "mock"
+
+
+@pytest.mark.parametrize(
+    ("attr", "value", "expected"),
+    (
+        ("color", "default", "default"),
+        ("color", None, None),
+        ("color", "invalid", None),
+        ("fontType", "monospace", "monospace"),
+        ("fontType", None, None),
+        ("fontType", "invalid", None),
+        ("highlight", False, False),
+        ("highlight", None, None),
+        ("highlight", "invalid", True),
+        ("isSubtle", False, False),
+        ("isSubtle", None, None),
+        ("isSubtle", "invalid", True),
+        ("italic", False, False),
+        ("italic", None, None),
+        ("italic", "invalid", True),
+        ("size", "small", "small"),
+        ("size", None, None),
+        ("size", "invalid", None),
+        ("strikethrough", False, False),
+        ("strikethrough", None, None),
+        ("strikethrough", "invalid", True),
+        ("underline", False, False),
+        ("underline", None, None),
+        ("underline", "invalid", True),
+        ("weight", "bolder", "bolder"),
+        ("weight", None, None),
+        ("weight", "invalid", None),
+    ),
+)
+def test_optional_setters(
+    textrun: TextRun,
+    attr: str,
+    value: str | bool | None,
+    expected: str | None,
+) -> None:
+    assert getattr(textrun, attr) is None
+    getattr(textrun, f"set_{attr}")(value)
+    assert getattr(textrun, attr) == expected
+
+
+@pytest.mark.parametrize(
+    ("attr", "value"),
+    (
+        ("color", "dark"),
+        ("fontType", "monospace"),
+        ("highlight", True),
+        ("isSubtle", True),
+        ("italic", True),
+        ("size", "small"),
+        ("strikethrough", True),
+        ("underline", True),
+        ("weight", "bolder"),
+    ),
+)
+def test_keyword_arguments(attr: str, value: str | bool | int) -> None:
+    param = {attr: value}
+    result = TextRun("mock", **param)
+    assert getattr(result, attr) == value
+
+
+def test_text_positional() -> None:
+    newobj = TextRun("test")
+    assert newobj.text == "test"
+
+
+def test_text_positional_requires_truthy() -> None:
+    with pytest.raises(ValueError, match="'text' is required"):
+        _ = TextRun("")
+
+
+def test_invalid_keyword_raises() -> None:
+    with pytest.raises(KeyError, match="Invalid keyword arguement"):
+        TextRun("mock", invalid="Test")
